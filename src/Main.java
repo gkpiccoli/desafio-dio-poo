e107ee4083f667f8d3a364a3d22c1e5f39cc9228
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-04-28 19:24:26 -0300

    Classe curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
     }
 }
