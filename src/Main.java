c0133b63a467d975ca5b7c0e31182436e7d488e5
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-04-29 08:13:56 -0300

    Modificadores de Acesso

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,39 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
 
         curso1.setTitulo("Curso Java");
-        curso1.setDescricao(" ");
+        curso1.setDescricao("Descrição do Curso");
+        curso1.setCargaHoraria(12);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Flutter");
+        curso2.setDescricao("Descrição do Curso");
+        curso2.setCargaHoraria(18);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição da Mentoria Java");
+        mentoria1.setData(LocalDate.of(2022,7,12));
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria Flutter");
+        mentoria2.setDescricao("Descrição da Mentoria Flutter");
+        mentoria2.setData(LocalDate.now());
+
+
+
 
         System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
+
     }
 }
